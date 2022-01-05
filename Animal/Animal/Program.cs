d67746ee41f234f2085a514659b16ece3418afdb
Dovlet <dovlet.cyka@gmail.com>
2022-01-05 11:01:22 +0200

    edit program file

--- Animal/Animal/Program.cs
+++ Animal/Animal/Program.cs
@@ -6,7 +6,7 @@ namespace Animal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Actions!");
         }
     }
 }
